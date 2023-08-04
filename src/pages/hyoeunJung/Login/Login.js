82727009ae65dedde29ff36f0d9b1f25aa132229
정효은 <junghyoeun@jeonghyoeun-ui-MacBookPro.local>
2023-08-04 17:09:06 +0900

    westagram github 과제

---
+++ src/pages/hyoeunJung/Login/Login.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./Login.scss";
+
+import { Link } from "react-router-dom";
+
+// 위에거 맞는지 모르겠음 밑에 버튼에 링크넣음 3-4-1. Link 컴포넌트 사용하기
+// 2가지 방법 1.함수 2.링크연결(?) 몰루??
+
+const Login = () => {
+  return (
+    <div className="login-container">
+      <div className="logo">westagram</div>
+      <div className="form">
+        <input
+          className="input-wrap"
+          type="text"
+          placeholder="전화번호, 사용자 이름 또는 이메일"
+        ></input>
+        <input
+          className="input-wrap"
+          type="password"
+          placeholder="비밀번호"
+        ></input>
+      </div>
+      <div className="btn-login">
+        <button className="btn" type="button">
+          {/* <a className="btn-deco" href="/main">
+            로그인
+          </a> */}
+          <Link className="btn-deco" to="/hyoeunjung-main">
+            로그인
+          </Link>
+        </button>
+        <div className="re-password-wrap">
+          <a className="re-password" href="#">
+            비밀번호를 잊으셨나요?
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/pages/hyoeunJung/Main/Main.js
@@ -0,0 +1,122 @@
+import React from "react";
+import "./Main.scss";
+
+const Main = () => {
+  return (
+    // nav 바
+    <div className="main">
+      <nav className="nav">
+        <div className="main-logo">
+          <img className="nav-images" src="/images/hyoeunJung/instagram.png" />
+          <h3 className="nav-font">WESTAGRAM</h3>
+        </div>
+        <input className="nav-input" type="text" placeholder="검색" />
+        <div className="imgsElements">
+          <img className="nav-images" src="/images/hyoeunJung/compass.png" />
+          <img className="nav-images" src="/images/hyoeunJung/heart.png" />
+          <img className="nav-images" src="/images/hyoeunJung/user.png" />
+        </div>
+      </nav>
+      <main>
+        <div>
+          {/* 왼쪽편 */}
+          {/* 피드이미지 */}
+          <div className="top-wrap">
+            <div className="user-name">
+              <img
+                className="top-user"
+                src="/images/hyoeunJung/sand2.png"
+                alt="동그란이미지사진"
+              />
+              <div>
+                <p>sand1111/</p>
+                <p>wecode-위코드</p>
+              </div>
+            </div>
+            <img src="/images/hyoeunJung/forward.png" />
+          </div>
+          <img className="sand" src="/images/hyoeunJung/sand1.png" />
+
+          {/* 피드 밑 아이콘 */}
+          <div className="imgsElements">
+            <img className="images" src="/images/hyoeunJung/heart.png" />
+            <img className="images" src="/images/hyoeunJung/msg.png" />
+            <img className="images" src="/images/hyoeunJung/download.png" />
+          </div>
+
+          {/*피드 아이곤 아래 좋아합니다 부분*/}
+
+          <div>
+            <img src="" alt="동그란사진" />
+            <p>님 외 4명이좋아합니다</p>
+            <p>
+              sand ㅇㅁㄴㄹㄴㅁㅇㄹㅇㅁㅇ<a> 더 보기</a>
+            </p>
+          </div>
+          <input type="text" placeholder="댓글 달기..." />
+          <button>게시</button>
+        </div>
+        {/* 오른쪽부분 */}
+        <div>
+          <div>
+            <img src="" alt="동그란사진" />
+            <p>wecode_bootcamp</p>
+            <p>WeCode위코드</p>
+          </div>
+          {/* 스토리좋아요박스 */}
+          <div>
+            <p>스토리</p>
+            <div>모두 보기</div>
+          </div>
+
+          <div>
+            <img src="" alt="동그란사진" />
+            <p>1번</p>
+            <p>시간 전</p>
+          </div>
+          <div>
+            <img src="" alt="동그란사진" />
+            <p>1번</p>
+            <p>시간 전</p>
+          </div>
+          <div>
+            <img src="" alt="동그란사진" />
+            <p>1번</p>
+            <p>시간 전</p>
+          </div>
+          {/* 회원님을위한추천 */}
+          <div>
+            <p>회원님을 위한 추천</p>
+            <p>모두 보기</p>
+          </div>
+          <div>
+            <div>
+              <p>im</p>
+              <p>oo님 외 3명이 팔...</p>
+            </div>
+
+            <button>팔로우</button>
+          </div>
+          <div>
+            <div>
+              <p>im</p>
+              <p>oo님 외 3명이 팔...</p>
+            </div>
+
+            <button>팔로우</button>
+          </div>
+          <div>
+            <div>
+              <p>im</p>
+              <p>oo님 외 3명이 팔...</p>
+            </div>
+
+            <button>팔로우</button>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Main;
