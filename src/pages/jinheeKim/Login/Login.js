ca2dc5aa8773a411b82ad3ce92d4a9b96c027017
jini <kjhee0394@gmail.com>
2023-08-07 13:17:52 +0900

    Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/jinheeKim/Login/Login.js
+++ src/pages/jinheeKim/Login/Login.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from 'react-router-dom';
 import "../../../style/reset.scss";
 import "../../../style/common.scss";
@@ -7,32 +7,40 @@ import "./Login.scss";
 
 
 const Login = () => {
+  const saveUserId = (e) => {
+};
+  const [id, setId] = useState('event.target.value');
+
+  const saveUserPw = (e) => {
+};
+  const [pw, setPw] = useState('event.target.value');
+
   return (
-    <>
-      <div className="login">
-        <div className="box">
-          <h1>Westagram</h1>
-          <div className="input-txt">
-            <input
-              type="text"
-              className="name"
-              placeholder="전화번호, 사용자 이름 또는 이메일"
-              />
-            <input
-              type="password"
-              className="pw"
-              placeholder="비밀번호"
-              />
-            <div className="login-btn">
-              <Link to="/jinheekim-main"><button>로그인</button></Link>
-            </div>
-            <div className="findPw">
-              <a href="#">비밀번호를 잊으셨나요?</a>
-            </div>
+    <div className="login">
+      <div className="box">
+        <h1>Westagram</h1>
+        <div className="input-txt">
+          <input
+            onChange={saveUserId} 
+            type="text"
+            className="id"
+            placeholder="전화번호, 사용자 이름 또는 이메일"
+            />
+          <input
+            onChange={saveUserPw}
+            type="password"
+            className="pw"
+            placeholder="비밀번호"
+            />
+          <div className="login-btn">
+            <Link to="/jinheekim-main"><button>로그인</button></Link>
+          </div>
+          <div className="findPw">
+            <a href="#">비밀번호를 잊으셨나요?</a>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
