d59d303eda2146b1b9450554468b485b2e9b6825
인님 <innim_e@jeonhyeongmin-ui-MacBookAir.local>
2023-08-09 21:50:37 +0900

    메인 페이지 전체 틀 작업중 & 오른쪽 하단 작업 완료

--- src/pages/hyungminJeon/Main/Main.js
+++ src/pages/hyungminJeon/Main/Main.js
@@ -18,16 +18,43 @@ const Main = () => {
         <span>|</span>
         <div></div>
       </nav>
-      {/* 메인페이지 왼쪽 */}
-      <div></div>
-      {/* 메인페이지 오른쪽 상단 */}
-      <div></div>
-      {/* 메인페이지 오른쪽 중간 */}
-      <div></div>
-      {/* 메인페이지 오른쪽 하단 */}
-      <div></div>
+      <div className="mainBody">
+        {/* 메인페이지 왼쪽 */}
+        <div className="bodyLeft">왼쪽</div>
+        {/* 메인페이지 오른쪽 */}
+        <div className="bodyRight">
+          오른쪽
+          {/* 메인페이지 오른쪽 상단 */}
+          <div>오른쪽 상단</div>
+          {/* 메인페이지 오른쪽 중간 */}
+          <div>오른쪽 중간</div>
+          {/* 메인페이지 오른쪽 하단 */}
+          <div className="divFooter">
+            {DIV_FOOTER.map((info) => (
+              <Link to="/" className="footerInfo">
+                {info}
+              </Link>
+            ))}
+            <span>2019 WESTAGRAM</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default Main;
+
+const DIV_FOOTER = [
+  "Westagram 정보 ",
+  "지원 ",
+  "홍보 센터 ",
+  "API ",
+  "채용 정보 ",
+  "개인정보처리방침 ",
+  "약관 ",
+  "디렉터리 ",
+  "프로필 ",
+  "해시태그 ",
+  "언어",
+];
